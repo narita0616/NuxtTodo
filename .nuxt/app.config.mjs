aa7074e164a42a793a3b9aad10c5d7955f56e159
narita_rikuto <naritari0616@icloud.com>
2023-06-04 22:51:35 +0900

    [NuxtTodo]プロジェクト作成

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/friendly/git/NuxtTodo/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
---
+++ components/AppAlert.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ message }}</h1>
+    <form>
+        <input type="text" placeholder="メッセージ" v-model=message />
+        <input type="submit" v-on:click.prevent="dispText" />
+    </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const message = ref("");
+
+const dispText  = () => {
+    console.log(message.value);
+}
+
+</script>
